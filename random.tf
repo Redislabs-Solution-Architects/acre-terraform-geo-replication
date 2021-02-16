a0471f35f6759facaf74a0a7dc52dd02ecdac916
DaShaun <dashaun@dashaun.com>
2021-02-15 23:25:22 -0600

    fix storage account naming

--- random.tf
+++ random.tf
@@ -6,4 +6,5 @@ resource "random_string" "resource_group_name" {
 resource "random_string" "storage_account_name" {
   length = 4
   special = false
+  upper = false
 }
--- storageAccount.tf
+++ storageAccount.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name = format("redisgeek-%s", random_string.storage_account_name.result)
+  name = format("redisgeek%s", random_string.storage_account_name.result)
   resource_group_name = azurerm_resource_group.resource_group.name
   location = azurerm_resource_group.resource_group.location
   account_tier = "Standard"
