7527298b6786ea01b42b3510696dc612c9fe7606
DaShaun <dashaun@redislabs.com>
2021-02-16 20:12:23 -0600

    set enforce private link endpoint network policies true

--- networks.tf
+++ networks.tf
@@ -7,8 +7,10 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = format("redisgeek-%s", random_string.subnet_name.result)
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes                               = ["10.0.1.0/24"]
+  enforce_private_link_endpoint_network_policies = true
+  name                                           = format("redisgeek-%s", random_string.subnet_name.result)
+  resource_group_name                            = azurerm_resource_group.resource_group.name
+  virtual_network_name                           = azurerm_virtual_network.vnet.name
+
 }
