86e3e73cab43a5421316e528b7a0611780a0a61c
DaShaun <dashaun@dashaun.com>
2021-03-05 14:45:49 -0600

    use location_1 remove dash from name

--- azurerm_resource_group.tf
+++ azurerm_resource_group.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "resource_group" {
-  name = format("redisgeek-%s",random_string.resource_group_name.result)
-  location = var.location
+  name = format("redisgeek%s",random_string.resource_group_name.result)
+  location = var.location_1
 
   tags = merge( var.tags, { name = "redisgeek" } )
 }
