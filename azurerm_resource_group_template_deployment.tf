8053216b7af12138a406b9cb26a55c9f2324a766
DaShaun <dashaun@dashaun.com>
2021-03-05 14:44:34 -0600

    rename files based on resource

--- azurerm_resource_group_template_deployment.tf
+++ azurerm_resource_group_template_deployment.tf
@@ -1,11 +1,11 @@
-resource "azurerm_resource_group_template_deployment" "acre" {
-  name = format("redisgeek-%s", random_string.acre_name.result)
+resource "azurerm_resource_group_template_deployment" "acre_1" {
+  name = format("redisgeek-%s", random_string.acre_name_1.result)
   resource_group_name = azurerm_resource_group.resource_group.name
   deployment_mode = var.acre_deployment_mode
   template_content = templatefile(var.acre_template_path,
   {
-    acre_name = format("redisgeek-%s", random_string.acre_name.result),
-    location = azurerm_resource_group.resource_group.location,
+    acre_name = format("redisgeek%s", random_string.acre_name_1.result),
+    location = var.location_1,
     acre_sku = var.acre_sku,
     acre_capacity = var.acre_capacity,
     acre_client_protocol = var.acre_client_protocol,
