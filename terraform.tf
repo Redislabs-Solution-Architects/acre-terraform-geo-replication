30681575034ef0f27f27261ea6e1e060eab294c5
DaShaun <dashaun@dashaun.com>
2021-03-05 14:47:30 -0600

    remove unused providers

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,8 @@
 terraform {
   required_providers {
-    github = {
-      source = "integrations/github"
-      version = "4.4.0"
-    }
     random = {
       source = "hashicorp/random"
       version = "3.0.1"
-    }
-    rediscloud = {
-      source = "RedisLabs/rediscloud"
-      version = "0.2.1"
-    }
-    tfe = {
-      version = "~> 0.24.0"
     }
     azurerm = {
       source = "hashicorp/azurerm"
