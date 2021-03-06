362bfa82f083ebc70a3eb5a982d358e05b14126a
DaShaun <dashaun@dashaun.com>
2021-03-05 21:06:05 -0600

    add linkedDatabases

--- azurerm_resource_group_template_deployment.tf
+++ azurerm_resource_group_template_deployment.tf
@@ -11,7 +11,9 @@ resource "azurerm_resource_group_template_deployment" "acre_1" {
     acre_client_protocol = var.acre_client_protocol,
     acre_eviction_policy = var.acre_eviction_policy,
     acre_cluster_policy = var.acre_cluster_policy,
-    acre_group_nickname = random_string.acre_group_name.result
+    acre_group_nickname = random_string.acre_group_name.result,
+    subscription_id = var.subscription_id,
+    resource_group_name = azurerm_resource_group.resource_group.name
   })
 
   depends_on = [azurerm_resource_group.resource_group]
@@ -30,10 +32,10 @@ resource "azurerm_resource_group_template_deployment" "acre_2" {
     acre_client_protocol = var.acre_client_protocol,
     acre_eviction_policy = var.acre_eviction_policy,
     acre_cluster_policy = var.acre_cluster_policy,
-    acre_group_nickname = random_string.acre_group_name.result
+    acre_group_nickname = random_string.acre_group_name.result,
+    subscription_id = var.subscription_id,
+    resource_group_name = azurerm_resource_group.resource_group.name
   })
 
-  depends_on = [
-    azurerm_resource_group_template_deployment.acre_1
-  ]
+  depends_on = [azurerm_resource_group_template_deployment.acre_1]
 }
