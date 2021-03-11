c3880ba46f2586ceb1522126153cfdcd4f3578e5
DaShaun <dashaun@dashaun.com>
2021-03-11 12:11:34 -0600

    add storage_container_name

--- random_string.tf
+++ random_string.tf
@@ -12,6 +12,13 @@ resource "random_string" "storage_account_name" {
   upper = false
 }
 
+resource "random_string" "storage_container_name" {
+  length = 4
+  number = false
+  special = false
+  upper = false
+}
+
 resource "random_string" "acre_name_1" {
   length = 4
   number = false
