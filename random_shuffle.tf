74ea7ef50ee56290e4a88573551f7aa0f252dd5b
DaShaun <dashaun@redislabs.com>
2021-05-06 11:33:19 -0500

    update regions list

--- random_shuffle.tf
+++ random_shuffle.tf
@@ -1,17 +1,25 @@
 resource "random_shuffle" "redisgeek" {
   # Regions with 3 AZs
+  # Region Pairs A -> B
+  # N Australia East -> Australia Southeast
+  # Y East Us 2 -> Central US
+  # Y East Us -> West US
+  # Y North Europe -> West Europe
+  # N North Central US -> South Central US
+  # N East Asia -> Southeast Asia
+  # N UK West -> UK South
+  # N West US 2 -> West Central US
   input = [
-    "Australia East",
-    "Central US",
-    "East US",
-    "North Europe",
-    "South Central US",
-    "Southeast Asia",
-    "UK South",
-    "East US 2",
-    "East US 2 EUAP",
-    "West Europe",
-    "West US 2"
+    "australiaeast",
+    "centralus",
+    "eastus",
+    "northeurope",
+    "southcentralus",
+    "southeastasia",
+    "uksouth",
+    "eastus2",
+    "westeurope",
+    "westus2"
   ]
   result_count = 2
 }
