9f8d321469de77e02b8a29de1370fb7467b2bd55
DaShaun <dashaun@redislabs.com>
2021-09-14 18:44:55 -0500

    remove deployment info from output

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,10 @@
 locals {
   redisgeek_config = {
-    acre_1              = azurerm_resource_group_template_deployment.acre_1.*
-    acre_2              = azurerm_resource_group_template_deployment.acre_2.*
     resource_group_name = azurerm_resource_group.resource_group.name
   }
 }
 
 output "redisgeek_config" {
   value     = jsonencode(local.redisgeek_config)
-  sensitive = true
+  sensitive = false
 }
